581d0ada2767d78fd96f4912a832d7623e09fb98
Tony <xx841995@gmail.com>
2024-05-18 21:16:54 +0800

    01 & 02 done

---
+++ src/S01_TwoSum/Solution.java
@@ -0,0 +1,40 @@
+package S01_TwoSum;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+	/**
+	 * 雙重迴圈暴力解
+	 */
+	public int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	/**
+	 * 最優解, 使用hashmap key儲存
+	 */
+	public int[] twoSum1(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i]; //計算補數
+			if (map.containsKey(complement)) { //若是補數在map中 就代表找到其解
+				return new int[] { map.get(complement), i };
+			}
+			map.put(nums[i], i); //key存方當前數 value為index
+		}
+
+		throw new IllegalArgumentException("No two sum solution");
+	}
+}
---
+++ src/S02_AddTwoNumber/Solution.java
@@ -0,0 +1,53 @@
+package S02_AddTwoNumber;
+
+import java.util.List;
+
+public class Solution {
+
+	public static class ListNode {
+
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode result = new ListNode();
+		ListNode head = result; //用於指針移動
+		int carry = 0; //進位值
+
+		while (l1 != null || l2 != null || carry != 0) {
+			int sum = carry; //將sum 初始為進位值
+
+			if (l1 != null) {
+				sum += l1.val;
+				l1 = l1.next;
+			}
+
+			if (l2 != null) {
+				sum += l2.val;
+				l2 = l2.next;
+			}
+
+			carry = sum / 10; //carry為int 故小數點會被截斷
+			sum %= 10; // 保留個位數
+
+			//創建新的ListNode並移動head
+			head.next = new ListNode(sum);
+			head = head.next;
+		}
+		return result.next; // result為空節點 故須回傳result.next
+	}
+
+}
