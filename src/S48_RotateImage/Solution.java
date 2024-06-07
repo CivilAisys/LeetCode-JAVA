f4fd493f8ec748a64cf57122e73fdd4b0b611bb5
Tony <xx841995@gmail.com>
2024-06-07 08:42:12 +0800

    48 done

---
+++ src/S48_RotateImage/Solution.java
@@ -0,0 +1,29 @@
+package S48_RotateImage;
+
+public class Solution {
+
+    /**
+     * 要將圖片進行90度翻轉 先進行1.轉置矩陣 2. 列翻轉
+     */
+    public void rotate(int[][] matrix) {
+        int matrixLength = matrix.length;
+
+        // 轉置矩陣
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = i; j < matrixLength; j++) { // j 從 i 開始 如果從0 開始。同一個元素會被交換兩次變成沒有變化
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // 列翻轉
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = 0; j < matrixLength / 2; j++) { // 只需要翻轉到中間位置即可
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrixLength - 1 - j]; // 需要 -1是因為矩陣邊長從1開始，矩陣索引從0開始
+                matrix[i][matrixLength - 1 - j] = temp;
+            }
+        }
+    }
+}
