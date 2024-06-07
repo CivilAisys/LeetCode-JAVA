d2cd911a0a445c5b3014df5f139f673853526bd8
TonyChen <tonychen@cherricorp.com>
2024-06-07 12:48:40 +0800

    53 done

--- src/S53_MaximumSubarray/Solution.java
+++ src/S53_MaximumSubarray/Solution.java
@@ -6,6 +6,14 @@ public class Solution {
      * 使用dp
      */
     public int maxSubArray(int[] nums) {
+        int currentSum = nums[0], maxSum = nums[0]; // 紀錄當前組合最大與全局最大
 
+        // 從 index 1 開始遍歷。遍歷每個數字有兩種選擇。拋棄前面數組重新開始或將當前加上前面數組 比較
+        for(int i = 1; i < nums.length; i++){
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;  
     }
 }
