12cdc6604869839a0f24ccbeb24f29b104bf1c70
Tony <xx841995@gmail.com>
2024-06-10 14:18:10 +0800

    56 done

---
+++ src/S56_MergeIntervals/Solution.java
@@ -0,0 +1,26 @@
+package S56_MergeIntervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public int[][] merge(int[][] intervals) {
+        // 先對區間進行排序 依照起始位置排
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            // 若merged為空或是當前區間與最後一個合併區間不重疊  直接加入
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                // 否則我們合併當前區間與最後一個合併後的區間
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
