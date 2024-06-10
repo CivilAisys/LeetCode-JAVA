a2206dbacb637a7786735fd7fb3328777c950c56
Tony <xx841995@gmail.com>
2024-06-10 21:24:42 +0800

    74 done

---
+++ src/S74_Searcha2DMatrix/Solution.java
@@ -0,0 +1,26 @@
+package S74_Searcha2DMatrix;
+
+public class Solution {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0, right = rows * cols - 1; // 將二維數組當成一維數組看待
+        // 二分查找
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / cols][mid % cols];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
