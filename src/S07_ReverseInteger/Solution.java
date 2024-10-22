3fb7bb9bdef3c1bbe3730662ae38d433a261c16f
Tony <xx841995@gmail.com>
2024-10-22 16:29:13 +0800

    07 refactor

--- src/S07_ReverseInteger/Solution.java
+++ src/S07_ReverseInteger/Solution.java
@@ -6,21 +6,24 @@ public class Solution {
 	 * java int會溢出
 	 */
 	public int reverse(int x) {
-		int num = Math.abs(x); // 先轉換成正整數 在最後在處理正負
 		int reversed = 0; // 反轉後的值
 
-		while (num != 0) {
-			int digit = num % 10; //取得個位數
-			num /= 10; // 除10移除個位數
+		while (x != 0) {
+			int digit = x % 10; // 取得個位數
+			x /= 10; // 除10移除個位數
 
 			// 在轉換前檢查是否會溢出
-			if (reversed > (Integer.MAX_VALUE - digit) / 10) {
-				return 0;
+			if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+				return 0; // 溢出返回0
 			}
+			if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+				return 0; // 溢出返回0
+			}
+
 			reversed = reversed * 10 + digit; // 轉換
 		}
 
-		return x < 0 ? -reversed : reversed;
+		return reversed;
 	}
 
 }
