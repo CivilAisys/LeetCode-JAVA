a15049aa385a4752e5a9565a9b528afa9a608a4e
Tony <xx841995@gmail.com>
2024-10-23 15:58:31 +0800

    19 refactor

--- src/S19_RemoveNthNodeFromEndofList/Solution.java
+++ src/S19_RemoveNthNodeFromEndofList/Solution.java
@@ -6,26 +6,29 @@ public class Solution {
 	 * 使用快慢指針  其中一個向前移n 接著遍歷到最後 那麼就是要移除的node了
 	 */
 	public ListNode removeNthFromEnd(ListNode head, int n) {
+		// 創建一個虛擬頭節點，指向head
 		ListNode dummy = new ListNode(0);
-		dummy.next = head; // 使用dummy保存head節點
+		dummy.next = head;
 
-		// 快慢指針皆指向dummy
-		ListNode first = dummy;
-		ListNode second = dummy;
+		// 初始化兩個指針
+		ListNode fast = dummy;
+		ListNode slow = dummy;
 
-		// 先讓first 向前移n
-		for (int i = 0; i < n; i++) {
-			first = first.next;
+		// 將 fast 指針向前移動 n + 1 步，這樣 fast 和 slow 之間的距離是 n + 1
+		for (int i = 0; i < n + 1; i++) {
+			fast = fast.next;
 		}
 
-		// 開始遍歷
-		while (first.next != null) {
-			first = first.next;
-			second = second.next;
+		// 同時移動 fast 和 slow，直到 fast 到達鏈表末尾
+		while (fast != null) {
+			fast = fast.next;
+			slow = slow.next;
 		}
-		// 此時second的下一個就是要刪除的節點了
-		second.next = second.next.next;
 
+		// 此時 slow 的下一個節點是要刪除的節點
+		slow.next = slow.next.next;
+
+		// 返回新的鏈表頭，dummy.next 是原始鏈表的頭節點（除非第一個節點被刪除）
 		return dummy.next;
 	}
 }
