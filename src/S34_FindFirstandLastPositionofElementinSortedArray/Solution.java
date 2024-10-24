5f5dad290769e62ec33f374b9dd3038eec8cecfd
Tony <xx841995@gmail.com>
2024-10-24 14:02:28 +0800

    34 refactor

--- src/S34_FindFirstandLastPositionofElementinSortedArray/Solution.java
+++ src/S34_FindFirstandLastPositionofElementinSortedArray/Solution.java
@@ -2,60 +2,60 @@ package S34_FindFirstandLastPositionofElementinSortedArray;
 
 public class Solution {
 
-    /*
-     * 使用兩個二分搜尋法  透過邊界切換的條件達成搜尋到的值是第一個還是第二個
-     */
-    public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[] { -1, -1 };
-        if (nums.length == 0) {
-            return result;
-        }
+	/*
+	 * 使用兩個二分搜尋法  透過邊界切換的條件達成搜尋到的值是第一個還是第二個
+	 */
+	public int[] searchRange(int[] nums, int target) {
+		int[] result = new int[] { -1, -1 };
+		if (nums.length == 0) {
+			return result;
+		}
 
-        result[0] = findFirst(nums, target);
-        result[1] = findLast(nums, target);
-        return result;
-    }
+		result[0] = findFirst(nums, target);
+		result[1] = findLast(nums, target);
+		return result;
+	}
 
-    private int findFirst(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+	private int findFirst(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+		int firstPosition = -1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            // 此條件為關鍵  代表mid可能為值的所在為left右側 最後當mid = left = right時指向的會是第一個出現的值
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        // 因為left = mid + 1 且內部會縮小距離  最後可能數組會越界
-        if (left < nums.length && nums[left] == target) {
-            return left;
-        }
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
 
-        return -1;
-    }
+			// 只要記錄最後一個出現target的位置就好
+			if (nums[mid] == target) {
+				firstPosition = mid;
+				right = mid - 1;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
 
-    private int findLast(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+		return firstPosition;
+	}
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+	private int findLast(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+		int lastPosition = -1;
 
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				lastPosition = mid;
+				left = mid + 1;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
 
-        if (right >= 0 && nums[right] == target) {
-            return right;
-        }
-
-        return -1;
-    }
+		return lastPosition;
+	}
 
 }
