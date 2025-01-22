97f972d2516e6366d4c1e0a42ded2c11aca317a5
Tony Chen <tony.chen@innotech.me>
2025-01-22 09:35:07 +0800

    787 done

--- src/S787_CheapestFlightsWithinKStops/Solution.java
+++ src/S787_CheapestFlightsWithinKStops/Solution.java
@@ -13,6 +13,18 @@ public class Solution {
 		}
 		dp[0][src] = 0; // 初始條件，從 src 出發，不經過任何中轉 代表在原地
 
-		return -1;
+		// 狀態轉移
+		for (int i = 1; i <= k + 1; i++) {
+			dp[i][src] = 0; // 無論訪問次數，src 到 src 的花費始終為 0
+			for (int[] flight : flights) {
+				int from = flight[0], to = flight[1], price = flight[2];
+				// 確保上一層（i-1 次訪問）可以到達 from
+				if (dp[i - 1][from] != Integer.MAX_VALUE) {
+					dp[i][to] = Math.min(dp[i][to], dp[i - 1][from] + price);
+				}
+			}
+		}
+
+		return dp[k + 1][dst] == Integer.MAX_VALUE ? -1 : dp[k + 1][dst];
 	}
 }
