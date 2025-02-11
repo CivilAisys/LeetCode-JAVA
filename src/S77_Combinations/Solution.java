794574add42506b41dbeac986419a37777154408
Tony Chen <tony.chen@innotech.me>
2025-02-11 17:18:06 +0800

    77 not done

---
+++ src/S77_Combinations/Solution.java
@@ -0,0 +1,15 @@
+package S77_Combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+	// 使用backTracking窮舉 但需要去重
+	public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> result = new ArrayList<>();
+		boolean[] used = new boolean[n]; // 紀錄是否當前數有被使用到
+
+		return null;
+	}
+}
