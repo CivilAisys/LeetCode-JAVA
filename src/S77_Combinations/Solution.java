436d06a37d1c93c97a31e43ec9540b4ebd0a5403
Tony Chen <tony.chen@innotech.me>
2025-02-12 08:19:50 +0800

    77 done

--- src/S77_Combinations/Solution.java
+++ src/S77_Combinations/Solution.java
@@ -8,8 +8,21 @@ public class Solution {
 	// 使用backTracking窮舉 但需要去重
 	public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> result = new ArrayList<>();
-		boolean[] used = new boolean[n]; // 紀錄是否當前數有被使用到
+		backTrack(result, new ArrayList<>(), n, k, 1);
+		return result;
+	}
 
-		return null;
+	private void backTrack(List<List<Integer>> result, List<Integer> path, int n, int k, int start) {
+		// 長度等於k 就加入結果
+		if (path.size() == k) {
+			result.add(new ArrayList<>(path));
+			return;
+		}
+
+		for (int i = start; i <= n; i++) {
+			path.add(i); // 將當前數字加入
+			backTrack(result, path, n, k, i + 1); // 遞增窮舉可以避免重複如 [1,2] [2,1]這樣的結果
+			path.remove(path.size() - 1); // 剪枝 將最後元素刪除
+		}
 	}
 }
